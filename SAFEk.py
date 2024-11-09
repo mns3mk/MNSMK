49946d52e33c427600947f41f4bc5359d57100bb
mns3mk <152816646+mns3mk@users.noreply.github.com>
2024-11-09 14:47:00 +0200

    Update SAFEk.py

--- SAFEk.py
+++ SAFEk.py
@@ -1,85 +1,73 @@
+import os
+os.system('pip install random')
+os.system('pip install websocket')
+os.system('pip install websocket-client')
+os.system('pip install requests')
 import websocket
 import ssl
 import os
 import json
 import gzip
-import time
+import requests
 from time import sleep
 import random
 import concurrent.futures
-import telebot
+
 created=0
 failed=0
 
 
-L = '\033[1;33m' 
-C = "\033[1;97m" 
-B = '\033[2;36m'
-Y = '\033[1;34m' 
-C = "\033[1;97m" 
-X = '\037' 
 G = '\033[1;32m'
-R = '\033[1;31m'
-id = "761058551"
+L = '\033[1;31m'
 
-token = "7517539122:AAFgeaW09EkEFh3fj6JUCJ0sjVqSlbtYWJg"
-bot = telebot.TeleBot(token)
-
-
+own_id= 761058551
+tele_bot='7517539122:AAFgeaW09EkEFh3fj6JUCJ0sjVqSlbtYWJg'
 ch='qwertyuioplkjhgfdsazxcvbnm'
 def create():
  global created
  global failed
- user=str(random.choice('qwertyuioplkjhgfdsazxcvbnm')[0])+str(''.join(random.choice(ch) for i in range(11)))
+ user=str(random.choice('qwertyuioplkjhgfdsazxcvbnm')[0])+str(''.join(random.choice(ch) for i in range(6)))
  
- tlg = f'''     
-`{user}`
-   '''
+ #user='kdvdfejevfsheh'
  
-
+ headers = {
+     "app": "com.safeum.android",
+     "host": None,
+     "remoteIp": "195.13.182.217",
+     "remotePort": str(8080),
+     "sessionId": "b6cbb22d-06ca-41ff-8fda-c0ddeb148195",
+     "time": "2023-04-30 12:13:32",
+     "url": "wss://195.13.182.217/Auth"
+ }
  
- headers = {"app": "com.safeum.android", "host": None, "remoteIp": "195.13.182.213",
-                                        "remotePort": str(8080), "sessionId": "b6cbb22d-06ca-41ff-8fda-c0ddeb148195",
-                                        "time": "2024-04-11 11:00:00", "url": "wss://51.79.208.190/Auth"}
  
+ data0={"action":"Register","subaction":"Desktop","locale":"en_GB","gmt":"+02","password":{"m1x":"503c73d12b354f86ff9706b2114704380876f59f1444133e62ca27b5ee8127cc","m1y":"6387ae32b7087257452ae27fc8a925ddd6ba31d955639838249c02b3de175dfc","m2":"219d1d9b049550f26a6c7b7914a44da1b5c931eff8692dbfe3127eeb1a922fcf","iv":"e38cb9e83aef6ceb60a7a71493317903","message":"0d99759f972c527722a18a74b3e0b3c6060fe1be3ad53581a7692ff67b7bb651a18cde40552972d6d0b1482e119abde6203f5ab4985940da19bb998bb73f523806ed67cc6c9dbd310fd59fedee420f32"},"magicword":{"m1x":"04eb364e4ef79f31f3e95df2a956e9c72ddc7b8ed4bf965f4cea42739dbe8a4a","m1y":"ef1608faa151cb7989b0ba7f57b39822d7b282511a77c4d7a33afe8165bdc1ab","m2":"4b4d1468bfaf01a82c574ea71c44052d3ecb7c2866a2ced102d0a1a55901c94b","iv":"b31d0165dde6b3d204263d6ea4b96789","message":"8c6ec7ce0b9108d882bb076be6e49fe2"},
+ "magicwordhint":"0000"
+ ,"login":str(user),"devicename":"Xiaomi Redmi Note 8 Pro",
+ "softwareversion":"1.1.0.1380","nickname":"hvtctchnjvfxfx"
+ ,"os":"AND"
+ ,"deviceuid":"c72d110c1ae40d50","devicepushuid":"*dxT6B6Solm0:APA91bHqL8wxzlyKHckKxMDz66HmUqmxCPAVKBDrs8KcxCAjwdpxIPTCfRmeEw8Jks_q13vOSFsOVjCVhb-CkkKmTUsaiS7YOYHQS_pbH1g6P4N-jlnRzySQwGvqMP1gxRVksHiOXKKP","osversion":"and_11.0.0","id":"1734805704"}
  
- data0={"action":"Register","subaction":"Desktop","locale":"en_GB","gmt":"+03","password":{"m1x":"7b04e83921d39450d82327792dbbf5d574785fe3d62b676de4111f5371fba237","m1y":"c7865524cefe571a48fd340a0c0518fe2547d0a9b86631fa6096c36dadc8dc7b","m2":"baefcf8122c389449f9becfc716e8362ea6e416a53fe1204c7fea5ca0d7214bf","iv":"47b034d9d06e9286f422ef09e8c4496b","message":"346921ace2270f72e09f0fded31273632571eee461314e7dea292daf2a7de9224fe4ed506df066089297c91dfb6c40d9a15856794af1fa90db9e42640755b1437fe71ccfec7df2f4dc09e3695dfc7954"},"magicword":{
-				"m1x":"4821be439c520d660dbeacb8cd6de5fddf12b80bf1a9e01b2966c604c34086ec","m1y":"7871a3e18f1c5711441bc30710d72d4e9b762d126609fd405e17d41bce580566"
-,"m2":"ee5841e1d2bb30120a2f53cd6810c2c56fad1df12090d55a24aaad1b347ad201",
-"iv":"efe711d355a1d8a80e0b45d93b5d6723","message":"a54c46093395bdca49dbe3758ddac3a6"}
-,"magicwordhint":"0000","login":str(user),"devicename":"Samsung SM-A127F","softwareversion":"1.1.0.2300","nickname":None,"os":"AND","deviceuid":"1eb6e1c129e559b4","devicepushuid":"*eYq74iDYToWi4D925ieNfK:APA91bEe9rT5-BYkFAU1y9Pt-AKDCNdHemdIkxA5OtC4rfdTasO55Vwl-YL3AGtdXWG9mZNL6Jr9Asr7t3-Cb8w_Oh5loGL3_OAZ5Ld1gbwOakaqPsDoZGkOYDWmPiw5win24foLaors","osversion":"and_13.0.0","id":"1533703124"}
- 
- ws=websocket.create_connection("wss://195.13.182.213/Auth", header=headers, sslopt={"cert_reqs": ssl.CERT_NONE})
+ ws=websocket.create_connection("wss://195.13.182.217/Auth", header=headers, sslopt={"cert_reqs": ssl.CERT_NONE})
  ws.send(json.dumps(data0))
  result=ws.recv()
  decoded_data = gzip.decompress(result)
+ #print(G+str(decoded_data))
  if '"comment":"Exists"' in str(decoded_data):
   failed+=1
  elif '"status":"Success"' in str(decoded_data):
   created+=1
-  with open('SafeUM.txt', 'a') as f:
-        f.write(user + ":sdam | TG : \n")
-
-  time.sleep(2)
-  bot.send_message(id, text=tlg,parse_mode="Markdown")
-
-
+  y = requests.post(f'https://api.telegram.org/bot{tele_bot}/sendmessage?chat_id={own_id}&text=`{user}`&parse_mode=markdown')
  elif '"comment":"Retry"' in str(decoded_data):
   failed+=1
  else:
   print(decoded_data)
 
-def send_results_periodically():
-    global created
-    global failed
 
-executor=concurrent.futures.ThreadPoolExecutor(max_workers=250)
-executor.submit(send_results_periodically)
-
+executor=concurrent.futures.ThreadPoolExecutor(max_workers=700)
 
 while True:
  executor.submit(create)
  os.system('clear')
- print(C+"safeUm app ")
  print(G+'Created : '+str(created))
- print(R+'Failed : '+str(failed))
+ print(L+'Failed : '+str(failed))
